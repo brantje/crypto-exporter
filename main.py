487d252eca97e3569a6e7c4842b48c5073967732
brantje <brantje@gmail.com>
2022-05-26 18:18:10 +0000

    Add pushgateway support

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import yaml
 import json
 from urllib.error import URLError, HTTPError
 from os import getenv
-from prometheus_client import start_http_server, Gauge, REGISTRY, PROCESS_COLLECTOR, PLATFORM_COLLECTOR, GC_COLLECTOR 
+from prometheus_client import start_http_server, Gauge, REGISTRY, PROCESS_COLLECTOR, PLATFORM_COLLECTOR, GC_COLLECTOR, push_to_gateway, CollectorRegistry 
 from time import sleep
 from pycoingecko import CoinGeckoAPI
 from cryptotools import Xpub
-
+registry = CollectorRegistry()
 [REGISTRY.unregister(c) for c in [PROCESS_COLLECTOR, PLATFORM_COLLECTOR, GC_COLLECTOR ]]
 
 
@@ -30,9 +30,10 @@ def loadConfig():
 CONFIG = loadConfig()
 cg = CoinGeckoAPI()
 
-WALLET_BALANCE = Gauge('crypto_wallet_balance','wallet_balance', ['currency','wallet'])
 
-EXCHANGE_RATE = Gauge('crypto_exchange_rate','Current exchange rates', ['currency', 'reference_currency'])
+WALLET_BALANCE = Gauge('crypto_wallet_balance','wallet_balance', ['currency','wallet'],  registry=registry)
+
+EXCHANGE_RATE = Gauge('crypto_exchange_rate','Current exchange rates', ['currency', 'reference_currency'], registry=registry)
 
 
 
@@ -117,6 +118,7 @@ def get_wallet_info(coin, wallet):
 
     display_name = CONFIG.get('coins', {}).get(coin, {}).get('display_name', coin).capitalize()
     WALLET_BALANCE.labels(currency=display_name, wallet=wallet).set(balance)
+    
 
 
 def coins_info(coins=[]):
@@ -126,7 +128,7 @@ def coins_info(coins=[]):
 
     reference_currency = CONFIG.get('currencies')
     price_data = cg.get_price(ids=coins_string, vs_currencies=reference_currency)    
-    
+   
     for coin in enabled_coins.keys():
         coin_price = price_data.get(coin, None)
         
@@ -137,6 +139,7 @@ def coins_info(coins=[]):
             display_name = c.get('display_name', coin).capitalize()
             for cur_ref in coin_price:
                 EXCHANGE_RATE.labels(currency=display_name, reference_currency=cur_ref).set(coin_price.get(cur_ref))
+                
         wallets = CONFIG.get('coins', {}).get(coin, {}).get('wallets', None)    
         if wallets:
             for wallet in wallets:
@@ -149,10 +152,14 @@ if __name__ == '__main__':
     logging.info(40 * '-')
    
     port = CONFIG.get('port')
-    start_http_server(port)
+    start_http_server(port, registry=registry)
     logging.info('Prometheus exporter running at port {}'.format(port))
     logging.info(40 * '-')
 
     while True:
         coins_info(CONFIG.get('coins', []))
+        if(CONFIG.get('pushgateway', []).get('enabled')):
+            push_gateway = CONFIG.get('pushgateway')
+            logging.info('Pushing to gateway {}'.format(push_gateway.get('host')))
+            push_to_gateway(push_gateway.get('host'), job=push_gateway.get('job'), registry=registry)
         sleep(60)
